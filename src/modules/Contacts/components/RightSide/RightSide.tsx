4b520b42f1fa4e79b9762436d90c3a82f95aeef8
Vlad Prusakov <vlad.prusakov1703@gmail.com>
2024-03-29 18:03:59 +0300

    fixed contacts

--- src/modules/Contacts/components/RightSide/RightSide.tsx
+++ src/modules/Contacts/components/RightSide/RightSide.tsx
@@ -5,7 +5,7 @@ import styles from './styles.module.scss';
 export const RightSide = ({ contacts }: { contacts: Contacts[] }) => {
   return (
     <div className={styles['right-side']}>
-      {contacts.map((el) => (
+      {contacts?.map((el) => (
         <Contact
           key={el.friend_address}
           address={el.friend_address}
