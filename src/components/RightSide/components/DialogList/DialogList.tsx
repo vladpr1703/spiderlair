54bc9ceffd0b8c0f19cd51aff77230ba5c4588e5
Vlad Prusakov <vlad.prusakov1703@gmail.com>
2024-03-29 18:01:43 +0300

    removed preloader

--- src/components/RightSide/components/DialogList/DialogList.tsx
+++ src/components/RightSide/components/DialogList/DialogList.tsx
@@ -23,19 +23,7 @@ export const DialogList = ({
             contactName={el.friend_address || el.friend_nickname || ''}
           />
         );
-      }) || (
-        <div className={styles.preloader}>
-          <ColorRing
-            visible={true}
-            height='80'
-            width='80'
-            ariaLabel='color-ring-loading'
-            wrapperStyle={{}}
-            wrapperClass='color-ring-wrapper'
-            colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
-          />
-        </div>
-      )}
+      })}
     </div>
   );
 };
